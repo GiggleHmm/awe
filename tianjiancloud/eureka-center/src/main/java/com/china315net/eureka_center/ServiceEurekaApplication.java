bca1056d7ebc90622ce0738ad9727c81afd19c2f
GiggleHmm <2066303812@qq.com>
2021-02-26 14:58:46 +0800

    测试

--- tianjiancloud/eureka-center/src/main/java/com/china315net/eureka_center/ServiceEurekaApplication.java
+++ tianjiancloud/eureka-center/src/main/java/com/china315net/eureka_center/ServiceEurekaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class ServiceEurekaApplication {
 
     public static void main(String[] args) {
+        System.out.print("hello github");
         SpringApplication.run(ServiceEurekaApplication.class, args);
     }
 
