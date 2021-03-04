4f1c76622095228c56e8f20ef3f4c59346f30e9d
GiggleHmm <2066303812@qq.com>
2021-03-04 19:33:47 +0800

    updata

--- tianjiancloud/service-zuul/src/main/java/com/china315net/servicezuul/ServiceZuulApplication.java
+++ tianjiancloud/service-zuul/src/main/java/com/china315net/servicezuul/ServiceZuulApplication.java
@@ -1,17 +1,15 @@
 package com.china315net.servicezuul;
 
 
-//import com.china315net.servicezuul.filter.RequestGlobalFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-//import org.springframework.context.annotation.Bean;
+
 
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableZuulProxy
 public class ServiceZuulApplication {
 
     public static void main(String[] args) {
@@ -20,19 +18,5 @@ public class ServiceZuulApplication {
 
 
 
-//   @Bean
-//    RequestGlobalFilter requestGlobalFilter(){
-//      return new RequestGlobalFilter();
-//    }
 
-//    @Bean
-//    TokenFilter tokenFilter() {
-//        return new TokenFilter();
-//    }
-
-
-//    @Bean
-//    ManagerFilter tokenFilter() {
-//        return new ManagerFilter();
-//    }
 }
